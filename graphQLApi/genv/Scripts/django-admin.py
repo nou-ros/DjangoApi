d5d2b67b08ae657fc6683880ec18129f1b7899a7
nou-ros <nouros44@gmail.com>
2020-03-06 22:47:59 +0600

    Added graphQLApi and restApi

---
+++ graphQLApi/genv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!f:\python\webdev~1\django\graphql\django~1\genv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ restApi/restenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!f:\python\webdev~1\django\django~1\django~3\profil~1\restenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
